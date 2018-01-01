987d491b55841ac470a100ecff74bd11a7bfd342
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-01 18:38:39 +0530

    Improved data attributes usage corresponding JS

--- Javascript/Exercise_1/compare_age.js
+++ Javascript/Exercise_1/compare_age.js
@@ -1,7 +1,7 @@
 //this is a user class that has name and age as its attributes
 function User(name, age) {
   this.name = name;
-  this.age = age;
+  this.age = parseInt(age);
 }
 
 //this function validate the ages
@@ -30,17 +30,23 @@ User.prototype.compare_age = function(user2) {
   else{
     older = this.age > user2.age ? this.name : user2.name;
     younger = this.age > user2.age ? user2.name : this.name;
-    alert(older + "is older than" + younger);
+    alert(older + " is older than " + younger);
   }
 }
 
-var button = document.getElementById("compare_age");
-var user = new Array();
+
+//this code initiates as and when user clicks on compare button
+var button = document.querySelector("[data-action = 'compare_age']");
 button.addEventListener('click', function () {
-  var user_names = document.querySelectorAll("input.user");
-  var user_ages = document.querySelectorAll("input.age");
-  for(var count = 0; count < user_names.length; count++){
-    user[count] = new User(user_names[count].value, parseInt(user_ages[count].value));
-  }
-  user[0].compare_age(user[1]);
+
+//this query selector fetches data attributes and DOM values
+  var user1_name = document.querySelector("[data-name = 'user1_name']").value;
+  var user1_age = document.querySelector("[data-name = 'user1_age']").value;
+  var user2_name = document.querySelector("[data-name = 'user2_name']").value;
+  var user2_age = document.querySelector("[data-name = 'user2_age']").value;
+
+//Initialization of two user objects and a function to compare the ages
+  user1 = new User(user1_name, user1_age);
+  user2 = new User(user2_name, user2_age);
+  user1.compare_age(user2);
 });
