c22e1f1f09edaf4d0ec51970a05bcfd3b59696f0
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-01 09:12:35 +0530

    Added data attributes in html

--- Javascript/Exercise_1/compare_age.js
+++ Javascript/Exercise_1/compare_age.js
@@ -14,29 +14,34 @@ User.prototype.isValidName = function(user2){
   return this.name.length != 0 && user2.name.length != 0 ? true : false
 }
 
-//This function compare ages
+//this function compare ages
 User.prototype.compare_age = function(user2) {
+  debugger;
+  var older = 0;
+  var younger = 0;
   if(!this.isValidAge(user2)){
     alert("Please enter valid ages for the users");
   }
   else if(!this.isValidName(user2)){
-    alert("Please enter valid user name")
+    alert("Please enter valid user name");
   }
   else if(this.age == user2.age){
     alert(this.name + " and " + user2.name + " are of the same age");
   }
   else{
-    var older = this.age < user2.age ? this.name : user2.name;
-    var younger = this.age < user2.age ? user2.name : this.name;
-    alert(older + " is older than " + younger);
+    older = this.age > user2.age ? this.name : user2.name;
+    younger = this.age > user2.age ? user2.name : this.name;
+    alert(older + "is older than" + younger);
   }
 }
 
 
 
-var button  = document.getElementById("compare_age");
+var button = document.getElementById("compare_age");
+var names = document.getElementsByName("user_name");
+var ages = document.getElementsByName("age");
 button.addEventListener('click', function () {
-  var user1 = new User(document.getElementById("user1_name").value,document.getElementById("user1_age").value);
-  var user2 = new User(document.getElementById("user2_name").value, document.getElementById("user2_age").value);
+  var user1 = new User(names[0].value, parseInt(ages[0].value));
+  var user2 = new User(names[1].value, parseInt(ages[1].value));
   user1.compare_age(user2);
 });
