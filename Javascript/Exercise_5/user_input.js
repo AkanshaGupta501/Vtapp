3cd2c8d9141256f7413a0c59082fd88820b13ee2
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-04 10:41:09 +0530

    Code Improvement

--- Javascript/Exercise_5/user_input.js
+++ Javascript/Exercise_5/user_input.js
@@ -1,9 +1,12 @@
 //This event is called when bosy is loaded and asks user to enter the values
 window.onload = function(){
-  firstname = prompt("Please enter the first name");
-  lastname = prompt("Please enter the last name");
-  var user_details = new User(firstname, lastname);
-  user_details.validateName();
+  var user_details;
+  do{
+    firstname = prompt("Please enter the first name");
+    lastname = prompt("Please enter the last name");
+    user_details = new User(firstname, lastname);
+  }while(!user_details.validateName());
+  user_details.displayUserName();
 }
 
 //User Class initialization with firstname and lastname as its attribute
@@ -17,28 +20,26 @@ User.prototype.validateName = function(){
   var isValid = true;
   var name_regex = new RegExp(/^[a-zA-Z\s]+$/);
   //Firstname and lastname must not be empty
-  if (this.firstName.length == 0 || this.firstName === ' '){
+    if(!isNaN(this.firstName)){
     isValid = false;
     alert("You must specify the first name to continue");
   }
-  else if(this.lastName.length == 0 || this.lastName === ' '){
+  else if(!isNaN(this.firstName)){
     isValid = false;
-    alert("you must specify the last name to continue");
+    alert("You must specify the last name to continue");
   }
   //Firstname and lastname must not consist of letters
   else if(!name_regex.test(this.firstName) || !name_regex.test(this.lastName)){
     isValid = false;
     alert("You must enter a valid name consisting of letters only");
   }
+  return isValid;
+}
 
-  //If name is valid display it, else display the error
-  if(isValid){
+//If name is valid display it as alert and inside the div
+User.prototype.displayUserName = function(){
     //trim the additional white spaces
     var displayMessage = "Hello " + this.firstName.trim() + " " + this.lastName.trim();
     alert(displayMessage)
     document.getElementById("message").innerHTML = displayMessage;
-  }
-  else{
-    document.getElementById("message").innerHTML = "Please refresh to start again";
-  }
 }
