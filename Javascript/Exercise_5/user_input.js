af8cb8a32ef5f3a922ee4e50be02ece7ddee0e7b
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-08 17:25:51 +0530

    Made improvements as suggested by mentor

--- Javascript/Exercise_5/user_input.js
+++ Javascript/Exercise_5/user_input.js
@@ -1,45 +1,56 @@
-//This event is called when bosy is loaded and asks user to enter the values
+//This event is called when body is loaded and asks user to enter the values
 window.onload = function(){
-  var user_details;
-  do{
-    firstname = prompt("Please enter the first name");
-    lastname = prompt("Please enter the last name");
-    user_details = new User(firstname, lastname);
-  }while(!user_details.validateName());
-  user_details.displayUserName();
+  var user_details = new User();
+  user_details.getUserName();
 }
 
 //User Class initialization with firstname and lastname as its attribute
-function User(firstname, lastname){
-  this.firstName  = firstname;
-  this.lastName = lastname;
+function User(){
+  this.firstName, this.lastName;
 }
 
-//validates name
-User.prototype.validateName = function(){
+User.prototype.validNameRegex = new RegExp(/^[a-zA-Z\s]+$/);
+
+User.prototype.messageBox = document.getElementById("message");
+
+User.prototype.getUserName = function(){
+  do {
+    this.firstName = prompt("Please enter the first name");
+    this.lastName = prompt("Please enter the last name");
+    } while(!this.validateUserName());
+    this.displayUserName();
+}
+
+User.prototype.validateUserName = function(){
   var isValid = true;
-  var name_regex = new RegExp(/^[a-zA-Z\s]+$/);
-  //Firstname and lastname must not be empty
-    if(!isNaN(this.firstName)){
+  if(!(this.validateNameIsNotEmpty(this.firstName, 'firstname') && this.validateNameIsNotEmpty(this.lastName, 'lastname'))){
     isValid = false;
-    alert("You must specify the first name to continue");
   }
-  else if(!isNaN(this.lastName)){
+  else if(!(this.validateNameHasLetters(this.firstName, 'firstname') && this.validateNameHasLetters(this.lastName, 'lastname'))){
     isValid = false;
-    alert("You must specify the last name to continue");
-  }
-  //Firstname and lastname must not consist of letters
-  else if(!name_regex.test(this.firstName) || !name_regex.test(this.lastName)){
-    isValid = false;
-    alert("You must enter a valid name consisting of letters only");
   }
   return isValid;
 }
 
+User.prototype.validateNameIsNotEmpty = function(name, nameType){
+  var isValid = isNaN(name) ? true : false;
+  if(!isValid){
+    alert("You must specify " + nameType);
+  }
+  return isValid;
+}
+
+User.prototype.validateNameHasLetters = function(name, nameType){
+  var isValidName = this.validNameRegex.test(name) ? true : false;
+  if(!isValidName){
+    alert("Your " + nameType + " must consist of letters only");
+  }
+  return isValidName;
+}
+
 //If name is valid display it as alert and inside the div
 User.prototype.displayUserName = function(){
-    //trim the additional white spaces
-    var displayMessage = "Hello " + this.firstName.trim() + " " + this.lastName.trim();
-    alert(displayMessage)
-    document.getElementById("message").innerHTML = displayMessage;
+  var messageText = "Hello " + this.firstName.trim() + " " + this.lastName.trim();
+  alert(messageText)
+  this.messageBox.innerHTML = messageText;
 }
