b387099dccc64a3f9a9650496ec83ae70d44c90e
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-09 12:48:54 +0530

    Improvement

---
+++ Javascript/Exercise_7/countries_selection.js
@@ -0,0 +1,29 @@
+function CountryNames() {
+  this.selectbox1 = document.getElementById("countrylist1");
+  this.selectbox2 = document.getElementById("countrylist2");
+  this.add = document.getElementById("add");
+  this.remove = document.getElementById("remove");
+
+  // method for onclick event
+  this.addOrRemoveEvent = function() {
+    var that = this;
+    this.add.onclick = function() { 
+      that.moveCountries(that.selectbox1, that.selectbox2); 
+    };
+    this.remove.onclick = function() { 
+      that.moveCountries(that.selectbox2, that.selectbox1); 
+    };
+  }
+
+  // method to move countries form one list to other
+  this.moveCountries = function(choice1, choice2) {
+    for (var i = 0; i < choice1.options.length; i++) {
+      if (choice1.options[i].selected) {
+        choice2.appendChild(choice1.options[i--]);
+      }
+    }
+  }
+}
+
+var countryList = new CountryNames();
+countryList.addOrRemoveEvent();
