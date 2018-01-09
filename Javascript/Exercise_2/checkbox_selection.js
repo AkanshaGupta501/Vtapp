e323775639eb36269740475d5d49c6b844dccd47
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-09 18:53:43 +0530

    to save changes

--- Javascript/Exercise_2/checkbox_selection.js
+++ Javascript/Exercise_2/checkbox_selection.js
@@ -1,47 +1,69 @@
-function Form(){
-  this.daysCheckboxes = document.querySelectorAll("[data-name = 'daysCheckbox']");
-  this.noneCheckbox = document.querySelector("[data-name = 'checkNone']");
-  
-  this.checkboxClickEvent = function(){
-    var current_reference = this;
-    for(var i = 0; i < this.daysCheckboxes.length; i++){
-      this.daysCheckboxes[i].onclick = function(){ current_reference.checkMaximumThreeCheckBoxes();}
-    }
+function Form(options){
+  this.daysCheckboxes = options.daysCheckBoxes;
+  this.noneCheckbox = options.noneCheckBox;
+  this.selectedDays = new Array();
+}
+
+Form.prototype.bindEventCheckboxClick = function(){
+  debugger;
+  for(var day = 0; day < this.daysCheckboxes.length; day++){
+    this.daysCheckboxes[day].onclick = function() { this.eventHandler(this.daysCheckboxes[day]); }
+  }
+
+  function eventHandler(checkBoxId){
+    this.validateCheckboxSelection(checkBoxId);
+  }
+} 
+
+Form.prototype.validateCheckboxSelection = function(checkBoxID){
+  //debugger;
+  if(this.checked){
+    this.noneCheckbox.selected = false;
+    this.selectedDays.push(checkBoxID.value);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// Form.prototype.checkboxClickEvent = function(){
+//     var current_reference = this;
+//     for(var day = 0; day < this.daysCheckboxes.length; day++){
+//       this.daysCheckboxes[day].onclick = function(){ 
+//         this.daysCheckboxes[day].checked ? this.selectedDays.pop() : this.selectedDays.push(this.daysCheckboxes[day].value);
+//         current_reference.checkMaximumThreeCheckBoxes();
+//       }
+//     }
     
-    this.noneCheckbox.onclick = function(){ current_reference.checkNone();}
-  }
+//     this.noneCheckbox.onclick = function(){ current_reference.checkNone();}
+//   }
 
-  this.checkMaximumThreeCheckBoxes = function(){
-    var totalDaysSelected = 0;
-    var nameOfSelectedDays = new Array();
+// Form.prototype.checkMaximumThreeCheckBoxes = function(){
+//       if(this.selectedDays.length > 3){
+//         // this.daysCheckboxes[selectedDay].checked = false;
+//         alert("You can Maximum select 3 days at a time. You have already selected " + this.selectedDays[2] + ", " + this.selectedDays[1] + ", " + this.selectedDays[0]);
+//         return false;
+//       }
+//     }
 
-    for(var selectedDay=0; selectedDay < this.daysCheckboxes.length; selectedDay++){
-      if(this.daysCheckboxes[selectedDay].checked === true){
-        this.noneCheckbox.checked = false;
-        nameOfSelectedDays.push(this.daysCheckboxes[selectedDay].value);
-        totalDaysSelected++;
-      }
-      
-      if(totalDaysSelected > 3){
-        this.daysCheckboxes[selectedDay].checked = false;
-        alert("You can Maximum select 3 days at a time. You have already selected " + nameOfSelectedDays[2] + ", " + nameOfSelectedDays[1] + ", " + nameOfSelectedDays[0]);
-        return false;
-      }
-    }
-  }
-
-  this.checkNone = function(){
-    for(var selectedDay = 0; selectedDay < this.daysCheckboxes.length; selectedDay++){
-      this.daysCheckboxes[selectedDay].checked = false;
-    }
-  }
-}
-
-Form.prototype.init = function(){
-  this.checkboxClickEvent();
-}
+// Form.prototype.checkNone = function(){
+//     for(var selectedDay = 0; selectedDay < this.daysCheckboxes.length; selectedDay++){
+//       this.daysCheckboxes[selectedDay].checked = false;
+//     }
+//   }
 
 window.onload = function(){
-  var form = new Form();
-  form.init();
+  var options = {
+    daysCheckBoxes : document.querySelectorAll("[data-name = 'daysCheckbox']"),
+    noneCheckBox : document.querySelector("[data-name = 'checkNone']")
+  };
+  console.log(options);
+  var newForm = new Form(options)
+  newForm.bindEventCheckboxClick();
 }
