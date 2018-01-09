f757b68bc6604c1ce4e11ef70453851507341c3e
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-09 18:05:19 +0530

    code improvement in JS-removed redundant code

--- Javascript/Exercise_6/url.js
+++ Javascript/Exercise_6/url.js
@@ -9,37 +9,35 @@ function Url(){
 }
 
 Url.prototype.init = function(){
-  this.getUrl();
-}
-
-Url.prototype.getUrl = function(){
-  do {
-    this.url = prompt("Please enter a URL address");
-  } while(!this.validateUrl());
+  this.getValidUrl();
   this.navigateToUrl();
 }
 
+Url.prototype.getValidUrl = function(){
+  do {
+    this.url = prompt("Please enter a URL address");
+  } while(!this.validateUrl()); 
+}
+
 Url.prototype.validateUrl = function(){
-  var isValid = (!this.checkIfNotEmptyAndValid()) ? false : true;
-  return isValid;
+  return (!this.checkIfNotEmptyAndValid()) ? false : true;
 }
 
 Url.prototype.checkIfNotEmptyAndValid = function(){
   var isUrlValid = true;
-  var isEmpty = !isNaN(this.url) ? false : true;
-  var isValidName = this.validUrlRegex.test(this.url) ? true : false;
-  if(!isEmpty){
+
+  if(!isNaN(this.url)){
     isUrlValid = false;
     alert("You must enter the URL to continue");
   }
-  else if(!isValidName){
+  else if(!this.ValidUrlRegex.test(this.url)){
     isUrlValid = false;
     alert("Your URL address is invalid.");
   }
   return isUrlValid;
 }
 
-Url.prototype.validUrlRegex = new RegExp(/(http(s)?:\/\/){1}([\da-z(\.|\-|\@|\:|\#|\_)?]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#%]{1}[[\da-z(\.|\-|\@|\:|\#|\_)?]+)*[\/\?]?/igm);
+Url.prototype.ValidUrlRegex = new RegExp(/(http(s)?:\/\/){1}([\da-z(\.|\-|\@|\:|\#|\_)?]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#%]{1}[[\da-z(\.|\-|\@|\:|\#|\_)?]+)*[\/\?]?/igm);
 
 Url.prototype.navigateToUrl = function(){
   window.open(this.url, "_blank", "width=400, height=450, menubar=0, scrollbars=0, status=0");
