0688e00870e97788211926e9aff621639d04aeb6
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-10 15:24:08 +0530

    Code Refactored

--- Javascript/Exercise_3/parent_checkbox_selection.js
+++ Javascript/Exercise_3/parent_checkbox_selection.js
@@ -1,49 +1,39 @@
-//Initialise the checkbox list class and call the expand function
-function CheckBoxList(parentLabel) {
-  this.parentbox = parentLabel;
-  this.expandChildCheckboxes(this);
+function CheckBoxList(options) {
+  this.pageDom = options;
 }
 
-CheckBoxList.prototype.expandChildCheckboxes = function(parentLabel){
-  debugger;
-  var parentCheckbox = parentLabel.parentbox.firstChild;
-  var subCheckboxesDiv = parentLabel.parentbox.nextSibling.nextSibling;
+CheckBoxList.prototype.bindEvent = function(){
+  var _this = this;
+  for(var i = 0; i < this.pageDom.length; i++){
+    this.pageDom[i].addEventListener("click", function(){
+      _this.toggleChildCheckboxes(this);
+    });
+  }
+}
+
+CheckBoxList.prototype.toggleChildCheckboxes = function(parentLabel){
+  var parentCheckbox = parentLabel.firstChild;
+  var subCheckboxesDiv = parentLabel.nextSibling.nextSibling;
   var subChildrenArray = subCheckboxesDiv.children;
   
-  //this condition checks whether parent checkbox is selected or not
+  //This condition checks whether parent checkbox is selected or not
   if(parentCheckbox.checked) {
-    //if parent is selected then all its children are set to true
-    subCheckboxesDiv.style.display="block";
+    subCheckboxesDiv.classList.add("showCheckboxes");
     parentCheckbox.scrollIntoView();
     for(position in subChildrenArray) {
-      subChildrenArray[position].firstChild.checked=true;
+      subChildrenArray[position].firstChild.checked = true;
     } 
   } 
   else {
-    //this COL hides the children checkboxes and turns their checked value to false
-    subCheckboxesDiv.style.display="none";
+    subCheckboxesDiv.classList.add("hideCheckboxes");
     for(position in subChildrenArray) {
-      subChildrenArray[position].firstChild.checked=false;
+      subChildrenArray[position].firstChild.checked = false;
     }
   }
 }
 
-var colors = document.querySelector("[data-name = 'colors']");
-colors.addEventListener('click', function(){
-  new CheckBoxList(this);
-});
-
-var movies = document.querySelector("[data-name = 'movies']");
-movies.addEventListener('click', function(){
-  new CheckBoxList(this);
-});
-
-var drinks = document.querySelector("[data-name = 'drinks']");
-drinks.addEventListener('click', function(){
-  new CheckBoxList(this);
-});
-
-var bikes = document.querySelector("[data-name = 'bikes']");
-bikes.addEventListener('click', function(){
-  new CheckBoxList(this);
-});
+window.onload = function() {
+  var nodeDOMS = document.querySelectorAll("[data-name = 'parent']");
+  var checkBox = new CheckBoxList(nodeDOMS);
+  checkBox.bindEvent();
+}
