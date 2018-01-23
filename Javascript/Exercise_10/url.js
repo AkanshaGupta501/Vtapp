da9cd5052e50aad637c26c2642cd054bd6d92d40
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-01-23 18:32:17 +0530

    Improved Variables and class name

--- Javascript/Exercise_10/url.js
+++ Javascript/Exercise_10/url.js
@@ -1,15 +1,15 @@
-function extractDomainSubdomain(options){
+function Url(options){
   this.submitButton = options.submitButton;
   this.url = options.urlField;
 }
 
-extractDomainSubdomain.prototype.validateURL = function() {  
+Url.prototype.validateURL = function() {  
   var isValid = this.UrlPattern.test(this.url.value);
-  isValid == false ? this.displayMessage("Please enter a valid url") : this.excerptDomainSubdomain();
+  isValid == false ? this.displayMessage("Please enter a valid url") : this.extractDomainSubdomain();
   return isValid;
 }
 
-extractDomainSubdomain.prototype.excerptDomainSubdomain = function(){
+Url.prototype.extractDomainSubdomain = function(){
   var hostName = this.url.value.match(this.UrlPattern);
   var domainName = hostName[2];
   var subDomainName = hostName[1];
@@ -20,7 +20,7 @@ extractDomainSubdomain.prototype.excerptDomainSubdomain = function(){
   
 }
 
-extractDomainSubdomain.prototype.bindEvent = function() {
+Url.prototype.bindEvent = function() {
   var _this = this;
   this.submitButton.addEventListener("click", function(){
   _this.validateURL() ? this.displayMessage("Form submitted") : event.preventDefault();
@@ -31,18 +31,18 @@ extractDomainSubdomain.prototype.bindEvent = function() {
   }); 
 }
 
-extractDomainSubdomain.prototype.displayMessage = function(message){
+Url.prototype.displayMessage = function(message){
   alert(message);
 }
 
-extractDomainSubdomain.prototype.UrlPattern = new RegExp(/^(?:http(?:s)?:\/\/)?(?:www\.)?((?:(?:[\w\-~]+)\.)*)((?:[\w\-~]+)\.[a-z]{2,6})(\/[\w#-~?]*)*$/i);
+Url.prototype.UrlPattern = new RegExp(/^(?:http(?:s)?:\/\/)?(?:www\.)?((?:(?:[\w\-~]+)\.)*)((?:[\w\-~]+)\.[a-z]{2,6})(\/[\w#-~?]*)*$/i);
 
 window.onload = function(){
   var options = {
     urlField : document.querySelector("[data-id = 'url']"),
     submitButton : document.querySelector("[data-id = 'submit']")
   };
-  var urlObject = new extractDomainSubdomain(options);
+  var urlObject = new Url(options);
   urlObject.bindEvent();
 }
 
