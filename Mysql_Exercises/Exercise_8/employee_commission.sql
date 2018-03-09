4745368814f97e2a655257717263ffbc41ff39e5
AkanshaGupta501 <akanshagupta217@gmail.com>
2018-03-09 11:16:35 +0530

    Keywords updated and removed group concat

--- Mysql_Exercises/Exercise_8/employee_commission.sql
+++ Mysql_Exercises/Exercise_8/employee_commission.sql
@@ -2,13 +2,13 @@
 **Ques - 1 Find the employee who gets highest total commission**
 
 mysql> SELECT employees.name, SUM(commission_amount) AS Total_Amount
-    -> FROM employees LEFT JOIN commissions
-    -> ON employee_id = employees.id
-    -> GROUP BY employee_id
-    -> HAVING Total_Amount = (SELECT MAX(total) FROM (
-    -> SELECT sum(commission_amount) AS total
-    -> FROM commissions
-    -> GROUP BY employee_id) HighestCommission);
+       FROM employees LEFT JOIN commissions
+       ON employee_id = employees.id
+       GROUP BY employee_id
+       HAVING Total_Amount = (SELECT MAX(total) FROM (
+         SELECT sum(commission_amount) AS total
+         FROM commissions
+         GROUP BY employee_id) HighestCommission);
 +-------------+--------------+
 | name        | Total_Amount |
 +-------------+--------------+
@@ -20,12 +20,12 @@ mysql> SELECT employees.name, SUM(commission_amount) AS Total_Amount
 **Ques-2 Select employee with 4th highest salary**
 
 mysql> mysql> SELECT name FROM employees
-    -> INNER JOIN (
-    -> SELECT salary FROM employees
-    -> GROUP BY salary
-    -> ORDER BY salary DESC
-    -> LIMIT 1 offset 3) FourthHighest
-    -> ON employees.salary = FourthHighest.salary;
+       INNER JOIN (
+         SELECT salary FROM employees
+         GROUP BY salary
+         ORDER BY salary DESC
+         LIMIT 1 offset 3) FourthHighest
+       ON employees.salary = FourthHighest.salary;
 +--------------+
 | name         |
 +--------------+
@@ -38,20 +38,20 @@ mysql> mysql> SELECT name FROM employees
 **Ques-3 Find the department that is giving highest commission**
 
 mysql> SELECT departments.name AS Department,
-     Total_Commission FROM departments
-     LEFT JOIN (
-     SELECT employees.department_id, sum(commission_amount) AS Total_Commission
-     FROM employees
-     LEFT JOIN commissions
-     ON employee_id = employees.id
-     GROUP BY employees.department_id) AS departments_commission
-     ON departments.id = departments_commission.department_id
-     GROUP BY Total_Commission
-     having Total_Commission = (SELECT MAX(total) FROM (
-     SELECT sum(commission_amount) AS total 
-       FROM employees LEFT JOIN commissions
-     ON employee_id = employees.id
-     GROUP BY employees.department_id) HighestCommission);
+       Total_Commission FROM departments
+       LEFT JOIN (
+         SELECT employees.department_id, sum(commission_amount) AS Total_Commission
+         FROM employees
+         LEFT JOIN commissions
+         ON employee_id = employees.id
+         GROUP BY employees.department_id) AS departments_commission
+       ON departments.id = departments_commission.department_id
+       GROUP BY Total_Commission
+       having Total_Commission = (SELECT MAX(total) FROM (
+         SELECT sum(commission_amount) AS total 
+         FROM employees LEFT JOIN commissions
+         ON employee_id = employees.id
+         GROUP BY employees.department_id) HighestCommission);
 +------------+------------------+
 | Department | Total_Commission |
 +------------+------------------+
@@ -62,11 +62,11 @@ mysql> SELECT departments.name AS Department,
 **Ques-4 Find employees with commission greater than Rs.3000**
 
 mysql> SELECT group_concat(name) AS Employees,
-    -> commission_amount AS Commission_Paid FROM
-    -> employees join commissions
-    -> ON employee_id = employees.id
-    -> GROUP BY commission_amount
-    -> HAVING commission_amount > 3000;
+       commission_amount AS Commission_Paid FROM
+       employees join commissions
+       ON employee_id = employees.id
+       GROUP BY commission_amount
+       HAVING commission_amount > 3000;
 +--------------------------+-----------------+
 | Employees                | Commission_Paid |
 +--------------------------+-----------------+
@@ -119,38 +119,38 @@ mysql> SELECT * FROM commissions;
 9 rows in set (0.02 sec)
 
 
-mysql> create database employee_commission;
+mysql> CREATE DATABASE employee_commission;
 Query OK, 1 row affected (0.01 sec)
 
-mysql> use employee_commission;
+mysql> USE employee_commission;
 Database changed
-mysql> create table departments(
-    -> id int auto_increment,
-    -> name varchar(30) not null,
-    -> primary key(id));
+mysql> CREATE TABLE departments(
+    -> id INT AUTO_INCREMENT,
+    -> name VARCHAR(30) NOT NULL,
+    -> PRIMARY KEY(id));
 Query OK, 0 rows affected (0.02 sec)
 
-mysql> create table employees(
-    -> id int auto_increment,
-    -> name varchar(30) not null,
-    -> salary int(100) default 0,
-    -> department_id int not null,
-    -> primary key(id));
+mysql> CREATE TABLE employees(
+    -> id INT AUTO_INCREMENT,
+    -> name VARCHAR(30) NOT NULL,
+    -> salary INT(100) DEFAULT 0,
+    -> department_id INT NOT NULL,
+    -> PRIMARY KEY(id));
 Query OK, 0 rows affected (0.02 sec)
 
-mysql> create table commissions(
-    -> id int auto_increment,
-    -> employee_id int not null,
-    -> commission_amount int(100) not null,
-    -> primary key(id),
-    -> foreign key(employee_id) references employees(id));
+mysql> CREATE TABLE commissions(
+    -> id INT AUTO_INCREMENT,
+    -> employee_id INT NOT NULL,
+    -> commission_amount INT(100) NOT NULL,
+    -> PRIMARY KEY(id),
+    -> FOREIGN KEY(employee_id) REFERENCES employees(id));
 Query OK, 0 rows affected (0.01 sec)
 
-mysql> alter table employees add foreign key(department_id) references departments(id);
+mysql> ALTER TABLE employees ADD FOREIGN KEY(department_id) REFERENCES departments(id);
 Query OK, 0 rows affected (0.03 sec)
 Records: 0  Duplicates: 0  Warnings: 0
 
-mysql> describe departments;
+mysql> DESCRIBE departments;
 +-------+-------------+------+-----+---------+----------------+
 | Field | Type        | Null | Key | Default | Extra          |
 +-------+-------------+------+-----+---------+----------------+
@@ -159,7 +159,7 @@ mysql> describe departments;
 +-------+-------------+------+-----+---------+----------------+
 2 rows in set (0.02 sec)
 
-mysql> describe employees;
+mysql> DESCRIBE employees;
 +---------------+-------------+------+-----+---------+----------------+
 | Field         | Type        | Null | Key | Default | Extra          |
 +---------------+-------------+------+-----+---------+----------------+
@@ -170,7 +170,7 @@ mysql> describe employees;
 +---------------+-------------+------+-----+---------+----------------+
 4 rows in set (0.03 sec)
 
-mysql> describe commissions;
+mysql> DESCRIBE commissions;
 +-------------------+----------+------+-----+---------+----------------+
 | Field             | Type     | Null | Key | Default | Extra          |
 +-------------------+----------+------+-----+---------+----------------+
@@ -180,14 +180,14 @@ mysql> describe commissions;
 +-------------------+----------+------+-----+---------+----------------+
 3 rows in set (0.02 sec)
 
-mysql> insert into departments(name) values
+mysql> INSERT INTO departments(name) VALUES
     -> ('banking'),
     -> ('insurance'),
     -> ('service');
 Query OK, 3 rows affected (0.00 sec)
 Records: 3  Duplicates: 0  Warnings: 0
 
-mysql> insert into employees(name, salary, department_id) values
+mysql> INSERT INTO employees(name, salary, department_id) VALUES
     -> ('Chris Gayle', 1000000, 1),
     -> ('Michael Clark', '800000', 2),
     -> ('Rahul Dravid', 700000, 1),
@@ -197,7 +197,7 @@ mysql> insert into employees(name, salary, department_id) values
 Query OK, 6 rows affected (0.00 sec)
 Records: 6  Duplicates: 0  Warnings: 0
 
-mysql> insert into commissions(employee_id, commission_amount) values
+mysql> INSERT INTO commissions(employee_id, commission_amount) VALUES
     -> (1, 5000),
     -> (2, 3000),
     -> (3, 4000),
@@ -211,6 +211,7 @@ Records: 8  Duplicates: 0  Warnings: 0
 
 
 **creating index as commission_amount because its referenced most of the time**
-mysql> create index commission_amount on commissions(commission_amount);
+
+mysql> CREATE INDEX commission_amount ON commissions(commission_amount);
 Query OK, 0 rows affected (0.05 sec)
 Records: 0  Duplicates: 0  Warnings: 0
